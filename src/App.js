85a059c69b4325214228ddb0c42287cca41b2cff
Jorge Olarte <ingeniero@jorgeolarte.com>
2022-09-29 15:22:08 -0500

    Moving files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
+import Index from "./pages/Index";
+
 export default function App() {
-  return (
-    <div className='flex flex-col bg-red-400 text-white min-h-screen justify-center items-center '>
-      <h1 className='text-3xl font-bold underline '>Hello world!</h1>
-    </div>
-  );
+  return <Index />;
 }
